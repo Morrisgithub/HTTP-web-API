d1f772db2154c7d5277a53359d6e3a95b790f857
Moris Afram <moris.afram@learnet.se>
2024-11-29 14:24:13 +0100

    Add project files.

---
+++ web-API/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+       
+        await GetGitHubRepositories();
+    }
+
+ 
+    static async Task GetGitHubRepositories()
+    {
+        
+        var url = "https://api.github.com/orgs/dotnet/repos";
+
+        
+        using var client = new HttpClient();
+
+        
+        client.DefaultRequestHeaders.Add("User-Agent", "DotNetHttpClient");
+
+        try
+        {
+            
+            var response = await client.GetStringAsync(url);
+
+            
+            var repositories = JsonSerializer.Deserialize<Repository[]>(response);
+
+            
+            foreach (var repo in repositories)
+            {
+                
+                var pushedAt = repo.PushedAt.ToString("yyyy-MM-dd HH:mm:ss");
+
+                
+                var homepage = string.IsNullOrEmpty(repo.Homepage) ? "" : repo.Homepage;
+                var description = string.IsNullOrEmpty(repo.Description) ? "" : repo.Description;
+
+                
+                Console.WriteLine($"Name: {repo.Name}");
+                Console.WriteLine($"Homepage: {homepage}");
+                Console.WriteLine($"GitHub: {repo.HtmlUrl}");
+                Console.WriteLine($"Description: {description}");
+                Console.WriteLine($"Watchers: {repo.Watchers:n0}");
+                Console.WriteLine($"Last push: {pushedAt}");
+                Console.WriteLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+
+
+public class Repository
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("html_url")]
+    public string HtmlUrl { get; set; }
+
+    [JsonPropertyName("homepage")]
+    public string Homepage { get; set; }
+
+    [JsonPropertyName("watchers")]
+    public int Watchers { get; set; }
+
+    [JsonPropertyName("pushed_at")]
+    public DateTime PushedAt { get; set; }
+}
+
+
+
+
+
